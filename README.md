9220386c572bdfa70418daf6a854feef960c2a3a
Tylryan <Darkstar1!>
2021-01-01 19:29:37 -0600

    1/1/21 Doesn't record new Subjects. Only their values

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tracker
+---
+## What is this tracker used for?
+---
+This tracker is intended to track the amount of hours you spend each day studying/doing a specific subject/thing.
+
---
+++ hour_tracker.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import ast
+
+record_path = Path('record.json')
+
+with open(record_path) as file:
+    contents = file.read()
+    historical_records = ast.literal_eval(contents)
+
+    file.close()
+
+new_record = {}
+input = input('Type in the Subject, Date, Hours Studied, Minutes Studied. Delimiter = " ": ').split()
+subject = input[0]
+date = input[1]
+hours = round(int(input[2]) + (int(input[3]) / 60),2)
+print(hours)
+for subject in historical_records:
+    if subject not in historical_records:
+        new_record[subject] = [{'Date': '',
+                               'Hours': 0
+        }]
+        historical_records[subject] = new_record[subject]
+        new_record[subject] = [{'Date': date, 'Hours': hours}]
+        historical_records[subject].append(new_record[subject])
+
+    elif subject in historical_records:
+        new_record[subject] = [{'Date': date, 'Hours': hours}]
+        historical_records[subject].append(new_record[subject])
+
+print(new_record)
+
+
+with open('record.json', 'w') as file:
+    file.write(str(historical_records))
+    file.close()
+
+
+
+
+
